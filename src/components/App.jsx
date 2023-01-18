43bb15b5e637e6d3d0c4567a8df7563b1f3990f3
BartekJedliczka <bartekjedliczka@gmail.com>
2023-01-18 19:24:51 +0100

    update Add and Section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Section from './Section/Section';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    // const options = Object.keys(this.state);
+    return (
+      <div>
+        <Section title="Please leave feedback"></Section>
+      </div>
+    );
+  }
+}
+
+export default App;
