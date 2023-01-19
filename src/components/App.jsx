1933e7d2a59c71be9224b0c7703eb2d6e744c053
BartekJedliczka <bartekjedliczka@gmail.com>
2023-01-19 11:18:37 +0100

    update App.jsx create Notification.jsx and Notification.module.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
+import Notification from './Notification/Notification';
 
 class App extends Component {
   state = {
@@ -9,12 +10,27 @@ class App extends Component {
     bad: 0,
   };
 
+  onLeaveFeedback = event => {
+    const { name } = event.target;
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+
   render() {
+    // const { good, neutral, bad } = this.state;
     const options = Object.keys(this.state);
     return (
       <div>
-        <Section title="Please leave feedback"></Section>
-        <FeedbackOptions options={options} />
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Notification message="There is no feedback" />
+        </Section>
       </div>
     );
   }
