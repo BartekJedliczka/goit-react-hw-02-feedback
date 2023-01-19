3cd7ba67fa10a95879e0d28d6228f205da6abc30
BartekJedliczka <bartekjedliczka@gmail.com>
2023-01-19 13:51:25 +0100

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,36 +1,3 @@
-// import { Component } from 'react';
-// import css from './Statistics.module.css';
-// import PropTypes from 'prop-types';
-// // import AnimatedNumber from 'animated-number-react';
-
-// class Statistics extends Component {
-//   render() {
-//     const { good, neutral, bad, total } = this.props;
-//     return (
-//       <>
-//         <ul className={css.list}>
-//           <li className={css.item}>Good: {good}</li>
-//           <li className={css.item}>Neutral: {neutral}</li>
-//           <li className={css.item}>Bad: {bad}</li>
-//           <li className={css.item}>Total: {total}</li>
-//         </ul>
-// <p className={css.feedback}>Positive feedback: %</p>
-//       </>
-//     );
-//   }
-// }
-
-// Statistics.propTypes = {
-//   good: PropTypes.number.isRequired,
-//   neutral: PropTypes.number.isRequired,
-//   bad: PropTypes.number.isRequired,
-//   total: PropTypes.number.isRequired,
-//   positivePercentage: PropTypes.number.isRequired,
-//   formatValue: PropTypes.func,
-// };
-
-// export default Statistics;
-
 import { Component } from 'react';
 import css from './Statistics.module.css';
 import PropTypes from 'prop-types';
